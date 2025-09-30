933188a280ff9531ed1164b5c95346d551aff96e
25AYUSHRAI <ayushrai252005@gmail.com>
2025-09-30 14:14:32 +0530

    the ai services part is completed

---
+++ src/services/ai.service.js
@@ -0,0 +1,11 @@
+const {GoogleGenAI} = require('@google/genai')
+const ai = new GoogleGenAI({});
+async function generateResponse(content){
+    console.log(content)
+    const response = await ai.models.generateContent({
+        model:"gemini-2.0-flash",
+        contents: content,
+    })
+    return response.text;
+}
+module.exports = {generateResponse};
